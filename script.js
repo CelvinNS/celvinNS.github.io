a409e942c209f60472b94b87c4dd7a9b4c76fd9c
celvinNS <celvinnetaniel@gmail.com>
2023-03-29 15:57:27 +0700

    update ui di bagian navbar, tambah table jadwal, mengaktifkan fungsi form, dan memperbarui galeri

--- script.js
+++ script.js
@@ -79,3 +79,49 @@ backToTopButton.addEventListener("click", () => {
 });
 
 
+// validasi form 
+const form = document.querySelector('form');
+const scriptURL = form.action;
+
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+const alertBox = document.querySelector('.alert');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // mencegah form melakukan submit secara default
+
+  // ketika tombol submit diklik 
+  // tampilkan tombol loading, hilangkan tombol kirim 
+  btnLoading.classList.toggle('d-none');
+  btnKirim.classList.toggle('d-none');
+
+  // mengirimkan form ke Formspree
+  fetch(scriptURL, {
+    method: 'POST',
+    body: new FormData(form),
+    mode: 'no-cors' //menambahkan opsi mode : 'no-cors'
+  })
+    .then((response) => {
+      // tampilkan tombol kirim, hilangkan tombol loading
+      btnLoading.classList.toggle('d-none');
+      btnKirim.classList.toggle('d-none');
+
+      // tampilkan pemberitahuan pesan berhasil terkirim
+      alertBox.classList.toggle('d-none');
+
+      form.reset();
+      console.log('Success!', response);
+    })
+    .catch((error) => {
+      // tampilkan tombol kirim, hilangkan tombol loading
+      btnLoading.classList.toggle('d-none');
+      btnKirim.classList.toggle('d-none');
+      
+      // tampilkan pemberitahuan pesan gagal terkirim
+  alertBox.querySelector('.alert-text').innerHTML = 'Pesan Anda gagal terkirim. Silahkan coba kembali!';
+  alertBox.classList.add('alert-danger');
+  alertBox.classList.remove('d-none');
+
+  console.error(error);});
+
+});
